76d931dc6d8b2f6f0d5cf6e0938f7d0befcc4936
YaelBadian <yael@proteantecs.com>
2020-04-25 17:12:43 +0300

    finish first part

--- pretraining.py
+++ pretraining.py
@@ -7,6 +7,7 @@ class feature_statistics_class():
 
     def __init__(self):
         self.n_total_features = 0  # Total number of features accumulated
+        self.pucts = ['!', '@', '#', '.', ':', ',', '$', '&', '%', '$', '~', "'", '+', '=', '*', '^', '>', '<', ';', '``']
 
         # Init all features dictionaries
         self.word_ctag_count = OrderedDict()  # feature 100
@@ -17,9 +18,37 @@ class feature_statistics_class():
         self.ctag_count = OrderedDict()  # feature 105
         self.pword_ctag_count = OrderedDict()  # feature 106
         self.nword_ctag_count = OrderedDict()  # feature 107
-        self.len_word_count = OrderedDict()  # len of the word
+
+        self.len_word_count = OrderedDict()  # length of the word
+        self.upper_lower_number_count = OrderedDict()  # (1) starts with 0-Upper 1-Lower 2-number (2) have uppers (3) have lowers (4) have numbers
+        self.punctuation_starts_count = OrderedDict()  # (1) punc (2) starts with 0-Upper 1-Lower 2-number (3) after punc starts with 0-Upper 1-Lower 2-number
+        self.punctuation_count = OrderedDict()  # (1) punc (2) have uppers (3) have lowers (4) have numbers
+        self.num_of_uppers_count = OrderedDict()  # numbers of uppers
 
 
+    @staticmethod
+    def map_char(c):
+        if c.isupper():
+            return 0
+        elif c.islower():
+            return 1
+        elif c.isnumeric():
+            return 2
+        else:  # punc
+            return 3
+
+    @staticmethod
+    def map_word(word):
+        has_upper, has_lower, has_number = False, False, False
+        for c in word:
+            mapped = feature_statistics_class.map_char(c)
+            if mapped == 0:
+                has_upper = True
+            elif mapped == 1:
+                has_lower = True
+            elif mapped == 2:
+                has_number = True
+        return has_upper, has_lower, has_number
 
     def add_key_to_dict(self, key, dict):
         if key in dict:
@@ -65,15 +94,63 @@ class feature_statistics_class():
         key = (nword, ctag)
         self.add_key_to_dict(key, self.nword_ctag_count)
 
+    def fill_len_word_count(self, word, ctag):
+        key = (len(word), ctag)
+        self.add_key_to_dict(key, self.len_word_count)
+
+    def fill_upper_lower_number_count(self, word, ctag):
+        c = word[0]
+        has_upper, has_lower, has_number = feature_statistics_class.map_word(word[1:])
+        key = (feature_statistics_class.map_char(c), has_upper, has_lower, has_number, ctag)
+        self.add_key_to_dict(key, self.upper_lower_number_count)
+
+    def fill_punctuation_starts_count(self, word, ctag):
+        word = word.replace('.', '')
+        for c in word:
+            if feature_statistics_class.map_char(c) == 3:
+                mapped = word.split(c)[-1]
+                if len(mapped) > 0:  # ends with punc
+                    mapped = feature_statistics_class.map_char(word.split(c)[-1][0])
+                else:
+                    mapped = 3
+                key = (c, feature_statistics_class.map_char(word[0]), mapped, ctag)
+                self.add_key_to_dict(key, self.punctuation_starts_count)
+                return
+
+    def fill_punctuation_count(self, word, ctag):
+        word = word.replace('.', '')
+        for c in word:
+            if feature_statistics_class.map_char(c) == 3:
+                has_upper, has_lower, has_number = feature_statistics_class.map_word(word)
+                key = (c, has_upper, has_lower, has_number, ctag)
+                self.add_key_to_dict(key, self.punctuation_count)
+                return
+
+    def fill_num_of_uppers_count(self, word, ctag):
+        n = 0
+        for c in word:
+            if feature_statistics_class.map_char(c) == 0:
+                n += 1
+        key = (n, ctag)
+        self.add_key_to_dict(key, self.num_of_uppers_count)
+
     def fill_all_dicts(self, history):
         word, pptag, ptag, ctag, nword, pword = history
         self.fill_word_ctag_count(word, ctag)
-        self.fill_suffix_count(word, ctag)
-        self.fill_pptag_ptag_ctag_count(pptag, ptag, ctag)
-        self.fill_ptag_ctag_count(ptag, ctag)
-        self.fill_ctag_count(ctag)
-        self.fill_pword_ctag_count(pword, ctag)
-        self.fill_nword_ctag_count(nword, ctag)
+        if len(word) > 1:  ########## think about punctuation ############
+            self.fill_suffix_count(word, ctag)
+            self.fill_prefix_count(word, ctag)
+            self.fill_pptag_ptag_ctag_count(pptag, ptag, ctag)
+            self.fill_ptag_ctag_count(ptag, ctag)
+            self.fill_ctag_count(ctag)
+            self.fill_pword_ctag_count(pword, ctag)
+            self.fill_nword_ctag_count(nword, ctag)
+            self.fill_len_word_count(word, ctag)
+            self.fill_upper_lower_number_count(word, ctag)
+            self.fill_punctuation_starts_count(word, ctag)
+            self.fill_punctuation_count(word, ctag)
+            self.fill_num_of_uppers_count(word, ctag)
+
 
     @staticmethod
     def create_histories(line):
@@ -97,7 +174,8 @@ class feature_statistics_class():
                 for history in line_histories:
                     self.fill_all_dicts(history)
 
-        print(self.suffix_count)
+
+
 
 file_path = "data/train1.wtag"
 features = feature_statistics_class()
